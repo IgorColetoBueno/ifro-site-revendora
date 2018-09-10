27a1198db02093ec18277dad50c67f1498560568
Igor Bueno <igor_mcb@hotmail.com>
2018-09-10 00:52:58 -0400

    Tirando build e node_modules do repositorio

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import * as React from "react";
 import { Route, Switch } from "react-router";
 import "./assets/css/app.css";
 import Home from './components/Home';
-import Home1 from './components/Home.1';
 
 class App extends React.Component {
   public render() {
     return (
       <Switch>
         <Route exact={true} path="/" component={Home}/>
-        <Route exact={true} path="/1" component={Home1}/>
       </Switch>
     );
   }
